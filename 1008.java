dce3f19e49abefa3627f1170cc42b297084f9dbf
HanJungyoon <wjsdbs27@naver.com>
2019-08-04 17:57:26 +0900

    A/B

---
+++ 1008.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        double a = s.nextInt();
+        double b = s.nextInt();
+        double sum = a / b;
+        System.out.println(sum);
+    }
+}
